9dbfd64c7e2e8349a1b7eeb0913146f4ae2e104c
wangdi <18661230709@163.com>
2018-08-12 15:11:36 +0800

    #FileInputStream#

--- src/main/java/cn/com/git/leon/IODemo/IOUtil.java
+++ src/main/java/cn/com/git/leon/IODemo/IOUtil.java
@@ -1,6 +1,7 @@
 package cn.com.git.leon.IODemo;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
@@ -11,10 +12,15 @@ public class IOUtil {
     public static void main(String[] args) {
         File file = new File("D:\\newFile.txt");
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            String str = "exception";
-            fileOutputStream.write(str.getBytes());
-            fileOutputStream.close();
+//            FileOutputStream fileOutputStream = new FileOutputStream(file);
+//            String str = "exception";
+//            fileOutputStream.write(str.getBytes());
+//            fileOutputStream.close();
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            inputStream.read(bytes);
+            String str = new String(bytes,"GBK");
+            System.out.println(str);
         } catch (Exception e) {
             e.printStackTrace();
         }
