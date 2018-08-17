6526305545a7b5482145f392bfb3dd3c4844150a
wangdi <18661230709@163.com>
2018-08-17 17:17:24 +0800

    commit init

---
+++ src/main/java/cn/com/git/leon/thread/CustomThread.java
@@ -0,0 +1,11 @@
+package cn.com.git.leon.thread;
+
+/**
+ * Created by shinco on 2018/8/13.
+ */
+public class CustomThread implements Runnable{
+
+    public void run() {
+        Thread thread = Thread.currentThread();
+    }
+}
