3deb16e29c0a0c10c2e35061cb4f3612536a13f7
wangdi <18661230709@163.com>
2018-08-26 22:55:21 +0800

    #init#

---
+++ src/main/java/cn/com/git/leon/spring/AspectTest.java
@@ -0,0 +1,13 @@
+package cn.com.git.leon.spring;
+
+/**
+ * @author sirius
+ * @since 2018/8/25
+ */
+public class AspectTest {
+
+    public void showTime(){
+        System.out.println(System.currentTimeMillis());
+    }
+
+}
---
+++ src/main/java/cn/com/git/leon/springbootDemo/Application.java
@@ -0,0 +1,15 @@
+package cn.com.git.leon.springbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sirius
+ * @since 2018/8/26
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
