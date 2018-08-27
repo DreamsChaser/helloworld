088f225fb06567bfbed759188b010f45e7a110b2
wangdi <18661230709@163.com>
2018-08-27 17:38:24 +0800

    commit init

---
+++ src/main/java/cn/com/git/leon/collection/map/HashMapDemo.java
@@ -0,0 +1,14 @@
+package cn.com.git.leon.collection.map;
+
+import java.util.HashMap;
+
+/**
+ * @author sirius
+ * @since 2018/8/27
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put(1,"测试位置");
+    }
+}
