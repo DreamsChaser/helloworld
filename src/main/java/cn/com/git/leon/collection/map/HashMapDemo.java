018472d9c83c68becf1feec2240807c3cebe72c3
wangdi <18661230709@163.com>
2018-09-25 17:41:16 +0800

    commit init

--- src/main/java/cn/com/git/leon/collection/map/HashMapDemo.java
+++ src/main/java/cn/com/git/leon/collection/map/HashMapDemo.java
@@ -11,7 +11,6 @@ public class HashMapDemo {
     public static void main(String[] args) {
         int i = (int) (0.75*2);
         final HashMap  hashMap = new HashMap(2);
-        System.out.println(new Integer(5).hashCode());
         hashMap.put(5,"key5");
         hashMap.put(7,"key7");
         hashMap.put(3,"key3");
