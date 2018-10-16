92c3008365287e9f63f5d440a880c8b67ae79ec9
wangdi <18661230709@163.com>
2018-10-16 22:36:28 +0800

    #inti#

---
+++ src/main/java/cn/com/git/leon/collection/list/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package cn.com.git.leon.collection.list;
+
+import java.util.LinkedList;
+
+/**
+ * @author sirius
+ * @since 2018/10/16
+ */
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(null);
+        linkedList.add(null);
+        System.out.println(linkedList.size());
+    }
+}
