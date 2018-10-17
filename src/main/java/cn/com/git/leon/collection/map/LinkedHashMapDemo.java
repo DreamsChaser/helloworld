f75972ddf33276c5e337974fb82a776df80729a9
wangdi <18661230709@163.com>
2018-10-17 17:22:27 +0800

    commit init

---
+++ src/main/java/cn/com/git/leon/collection/map/LinkedHashMapDemo.java
@@ -0,0 +1,15 @@
+package cn.com.git.leon.collection.map;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author sirius
+ * @since 2018/10/17
+ */
+public class LinkedHashMapDemo {
+
+    public static void main(String[] args) {
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put(null,"123");
+    }
+}
