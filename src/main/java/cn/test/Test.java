fc5c426ac441a38bc3b0b877ef167a4dd3ca3d73
wangdi <18661230709@163.com>
2018-10-30 15:20:20 +0800

    commit init

--- src/main/java/cn/test/Test.java
+++ src/main/java/cn/test/Test.java
@@ -1,25 +1,7 @@
 package cn.test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-
 /**
  * @author sirius
  */
-public class Test {
-
-    public static void main(String[] args) {
-        Integer i = -129;
-        Integer j = -129;
-        System.out.println(i==j);
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
+public class Test{
 }
