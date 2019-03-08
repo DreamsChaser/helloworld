9cd378d04138fc34cd78f8b6213c811e2b99a607
wangdi <18661230709@163.com>
2019-03-08 17:34:51 +0800

    commit init

---
+++ src/main/java/三轮/B_JavaCore/f_innerClass/MemberInnerClass.java
@@ -0,0 +1,8 @@
+package 三轮.B_JavaCore.f_innerClass;
+
+/**
+ * @author sirius
+ * @since 2019/3/8
+ */
+public class MemberInnerClass {
+}
---
+++ src/main/java/三轮/B_JavaCore/f_innerClass/StaticInnerClass.java
@@ -0,0 +1,22 @@
+package 三轮.B_JavaCore.f_innerClass;
+
+/**
+ * @author sirius
+ * @since 2019/3/8
+ */
+public class StaticInnerClass {
+
+    int a;
+
+    final int b = 2;
+
+    static int c;
+
+    public static class Inner{
+
+        public void show(){
+            c = 10;
+        }
+
+    }
+}
