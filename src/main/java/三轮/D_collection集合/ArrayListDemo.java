959984392288dd4e90d728d63e04af5d50cd3e6e
wangdi <18661230709@163.com>
2019-03-15 17:40:21 +0800

    commit init

---
+++ src/main/java/三轮/D_collection集合/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package 三轮.D_collection集合;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sirius
+ * @since 2019/3/15
+ */
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
+    }
+}
