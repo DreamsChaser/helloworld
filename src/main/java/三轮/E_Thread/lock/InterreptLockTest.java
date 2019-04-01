c3f1f1d7729bb7a094c436daa4c320646aadb936
wangdi <18661230709@163.com>
2019-04-01 22:10:38 +0800

    '20190401'

---
+++ src/main/java/三轮/E_Thread/lock/InterreptLockTest.java
@@ -0,0 +1,11 @@
+package 三轮.E_Thread.lock;
+
+/**
+ * @author sirius
+ * @since 2019/4/1
+ */
+public class InterreptLockTest {
+    public static void main(String[] args) {
+        InterreptLockDemo interreptLockDemo = new InterreptLockDemo();
+    }
+}
