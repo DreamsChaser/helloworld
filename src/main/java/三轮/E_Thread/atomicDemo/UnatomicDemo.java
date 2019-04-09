4cebdb34d3e7128d26429262d0293abc654c85b5
wangdi <18661230709@163.com>
2019-04-09 20:58:49 +0800

    '20190409'

--- src/main/java/三轮/E_Thread/atomicDemo/UnatomicDemo.java
+++ src/main/java/三轮/E_Thread/atomicDemo/UnatomicDemo.java
@@ -3,14 +3,40 @@ package 三轮.E_Thread.atomicDemo;
 import java.util.concurrent.*;
 
 /**
- * 演示i++非原子操作
- * 线程1 2获取i的值，进行+1操作 阻塞5秒
+ * 线程之间变量不共享
  * @author sirius
  * @since 2019/4/9
  */
 public class UnatomicDemo {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+//            volatile int state = 0;
+            int state = 0;
+            @Override
+            public void run() {
+                if ("pool-1-thread-1".equals(Thread.currentThread().getName())){
+                    //线程1
+                    while (true){
+                        if (state == 10)
+                        break;
+                    }
+                    System.out.println("我好了");
+                }else if ("pool-1-thread-2".equals(Thread.currentThread().getName())){
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                        System.out.println("我来修改状态");
+                        state = 10;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(runnable);
+        executorService.submit(runnable);
+        executorService.shutdown();
     }
 }
